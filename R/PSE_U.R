5828d90cbfc8384745c2ddecb8f9ef0a3f6dcc36
Esther-R-code <82937782+Esther-R-code@users.noreply.github.com>
2021-09-22 11:24:55 +0800

    Delete PSE_U.R
    it's for sensitivity analysis

--- R/PSE_U.R
+++
@@ -1,6 +0,0 @@
-PSE_U<- function(PSElist, catecount, w_c, pathcount, dor){
-  if(dor=="D"){PSEu<- PSElist[[1]][pathcount,paste0("Outcome=",w_c[catecount])]
-  }else if(dor=="R"){PSEu<- PSElist[[2]][pathcount,paste0("Outcome=",w_c[catecount])]}
-  else{cat("The parameter 'dor' must be D or R, for idenfity which scale be used.")}
-  return(PSEu)
-}
